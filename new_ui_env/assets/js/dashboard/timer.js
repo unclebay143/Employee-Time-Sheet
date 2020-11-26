ada5b3727d6e657e6dbc959e6ed0751699d1d0a3
Samuel <unclebay143@gmail.com>
2020-11-26 19:11:28 +0100

    current version safe mode

--- new_ui_env/assets/js/dashboard/timer.js
+++ new_ui_env/assets/js/dashboard/timer.js
@@ -58,7 +58,7 @@ function timerReminder() {
         setTimeout(function() {
             swal({
                 title: "Timer reminder",
-                text: `You have logged in for 5 minutes, Did you forget to start your time?`,
+                text: `Did you forget to start your time?`,
                 icon: "warning",
                 button: "Okay",
             })
